a4fe0b8bb55c6cdeb2bfd9d68049290b4ff1c1f2
ekusuy <ekusuy.omi@gmail.com>
2022-11-12 00:07:43 +0900

    型追加

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 
 import { Position } from '../common/Position';
 
-export const initFields = (fieldSize: number, initialPosition: Position) => {
+export const initFields = (fieldSize: number, initialPosition: Position): string[][] => {
   const fields = [];
   for (let i = 0; i < fieldSize; i++) {
     const cols = new Array(fieldSize).fill("");
